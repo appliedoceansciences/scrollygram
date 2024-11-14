2fe56881d831c73e685e14f3238204d25af97e0d
Richard Campbell <richard.campbell@appliedoceansciences.com>
2024-11-14 05:01:56 -0800

    rev from upstream

--- pcm2packets.py
+++ pcm2packets.py
@@ -1,40 +1,42 @@
 #!/usr/bin/env python3
+# this is a quick and dirty script which ingests raw pcm in the expected format on stdin,
+# and emits the same data with logging and acoustic packet headers prepended s.t. packets
+# of not more than 1500 bytes are emitted, in the format expected by the dsp
 
 import struct
 import sys
-import numpy as np
 
-input_dtype = np.dtype('int32')
-sample_rate = 8000.0
-C = 4
+input_dtype_string = 'single'
+sample_rate = 24000.0
+C = 20
 t = 1725898437.0
 
 # loop over pairs of arguments
 for key, value in zip(sys.argv[1::2], sys.argv[2::2]):
     if key == 'fs': sample_rate = float(value)
     if key == 'C': C = int(value)
-    if key == 'dtype': input_dtype = np.dtype(value)
+    if key == 'dtype': input_dtype_string = value
     if key == 't0': t = float(value)
 
-# number of samples per packet is maximum number s.t. packet size is not more than 1500 bytes
-T = (1500 - 16) // (input_dtype.itemsize * C)
+itemsize = 2 if input_dtype_string == 'int16' else 4
 
-packet_size = 16 + input_dtype.itemsize * C * T
+# number of samples per packet is maximum number s.t. packet size is not more than 1500 bytes
+T = (1500 - 16) // (itemsize * C)
+
+packet_size = 16 + itemsize * C * T
 packet_size_with_padding = (packet_size + 7) & ~7
 
 seqnum = 0
-flags = 0b01 if input_dtype == np.int32 else 0b11 if input_dtype == np.float32 else 0b00
+flags = 0b01 if input_dtype_string == 'int32' else 0b11 if input_dtype_string == 'single' else 0b00
 
 samples_yielded = 0
 
 while True:
-    bytes = sys.stdin.buffer.read(input_dtype.itemsize * C * T)
-    if len(bytes) < input_dtype.itemsize * C * T: break
-
-    samples = np.ndarray(buffer=bytes, dtype=input_dtype, shape=[T, C])
+    bytes = sys.stdin.buffer.read(itemsize * C * T)
+    if len(bytes) < itemsize * C * T: break
 
     samples_yielded += T
-    timestamp_ticks = round(samples_yielded * 1e6 / (sample_rate * 16)) % 281474976710656
+    timestamp_ticks = round((t + samples_yielded / sample_rate) * 1e6 / 16) % 281474976710656
     timestamp_lsbs = (timestamp_ticks) & 65535
     timestamp_msbs = (timestamp_ticks) >> 16
 
@@ -47,7 +49,7 @@ while True:
 
     sys.stdout.buffer.write(packet_header_bytes)
 
-    sys.stdout.buffer.write(samples)
+    sys.stdout.buffer.write(bytes)
     sys.stdout.buffer.flush()
 
     seqnum = (seqnum + 1) % 65536
