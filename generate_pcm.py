735754bedff6ce3421627140163a7cda7028d2fa
Richard Campbell <richard.campbell@appliedoceansciences.com>
2024-12-19 15:22:49 -0800

    move standalone usage to if __name__ == '__main__'

--- generate_pcm.py
+++ generate_pcm.py
@@ -35,50 +35,51 @@ def generate_blocks_of_dithered_tone(T_per_block, C, tone_frequency, tone_amplit
 
         yield samples.astype(dtype)
 
-fs = 8000
-C = 1
-dtype = np.single
+if __name__ == '__main__':
+    fs = 8000
+    C = 1
+    dtype = np.single
 
-tone_frequency = 440
+    tone_frequency = 440
 
-tone_amplitude = 32766.0 / 32767.0
-tpdf_amplitude = 1.0 / 32767.0
+    tone_amplitude = 32766.0 / 32767.0
+    tpdf_amplitude = 1.0 / 32767.0
 
-throttle = 0
-duration = 0
+    throttle = 0
+    duration = 0
 
-# loop over pairs of arguments
-for key, value in zip(sys.argv[1::2], sys.argv[2::2]):
-    if key == 'fs': fs = float(value)
-    if key == 'C': C = int(value)
-    if key == 'tone_frequency': tone_frequency = float(value)
-    if key == 'tone_amplitude': tone_amplitude = float(value)
-    if key == 'tpdf_amplitude': tone_amplitude = float(value)
-    if key == 'throttle': throttle = float(value)
-    if key == 'duration': duration = float(value)
-    if key == 'dtype': dtype = np.dtype(value)
+    # loop over pairs of arguments
+    for key, value in zip(sys.argv[1::2], sys.argv[2::2]):
+        if key == 'fs': fs = float(value)
+        if key == 'C': C = int(value)
+        if key == 'tone_frequency': tone_frequency = float(value)
+        if key == 'tone_amplitude': tone_amplitude = float(value)
+        if key == 'tpdf_amplitude': tone_amplitude = float(value)
+        if key == 'throttle': throttle = float(value)
+        if key == 'duration': duration = float(value)
+        if key == 'dtype': dtype = np.dtype(value)
 
-T_per_block = 1024
+    T_per_block = 1024
 
-T = int(round(duration * fs))
-blocks_to_yield = T // T_per_block
-if duration > 0 and blocks_to_yield == 0: blocks_to_yield = 1
+    T = int(round(duration * fs))
+    blocks_to_yield = T // T_per_block
+    if duration > 0 and blocks_to_yield == 0: blocks_to_yield = 1
 
-t0 = time.monotonic()
-time_per_block = T_per_block / fs
+    t0 = time.monotonic()
+    time_per_block = T_per_block / fs
 
-child = generate_blocks_of_dithered_tone(T_per_block, C, tone_frequency, tone_amplitude, tpdf_amplitude, fs, dtype)
+    child = generate_blocks_of_dithered_tone(T_per_block, C, tone_frequency, tone_amplitude, tpdf_amplitude, fs, dtype)
 
-iblock = 0
-while iblock < blocks_to_yield or not blocks_to_yield:
-    if throttle:
-        now = time.monotonic()
-        if now < iblock * time_per_block / throttle + t0:
-            time.sleep((iblock * time_per_block / throttle + t0) - now)
+    iblock = 0
+    while iblock < blocks_to_yield or not blocks_to_yield:
+        if throttle:
+            now = time.monotonic()
+            if now < iblock * time_per_block / throttle + t0:
+                time.sleep((iblock * time_per_block / throttle + t0) - now)
 
-    samples = next(child)
+        samples = next(child)
 
-    sys.stdout.buffer.write(samples.astype(dtype))
-    sys.stdout.buffer.flush()
+        sys.stdout.buffer.write(samples.astype(dtype))
+        sys.stdout.buffer.flush()
 
-    iblock += 1
+        iblock += 1
