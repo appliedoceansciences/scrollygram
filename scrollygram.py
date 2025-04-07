5e5775ec6c3454d9ae68a8a34c7d32c983b62de6
Richard Campbell <richard.campbell@appliedoceansciences.com>
2025-04-07 13:15:23 -0700

    default to "shm:/cobs_to_shm" if "shm" is given

--- scrollygram.py
+++ scrollygram.py
@@ -133,7 +133,7 @@ def main():
         if 'test' in sys.argv[1]:
             input_source = None
             yield_packet_bytes_function = test_input
-        elif 'shm:' in sys.argv[1]:
+        elif 'shm' in sys.argv[1]:
             try:
                 from shared_memory_ringbuffer_reader import shared_memory_ringbuffer_generator
             except:
@@ -144,7 +144,7 @@ def main():
                 for packet_with_logging_header in shared_memory_ringbuffer_generator(source):
                     yield packet_with_logging_header[8:]
 
-            input_source = sys.argv[1].split(':')[1]
+            input_source = sys.argv[1].split(':')[1] if 'shm:' in sys.argv[1] else '/cobs_to_shm'
             yield_packet_bytes_function = yield_from_shm_and_strip_logging_header
         elif ':' in sys.argv[1]:
             address, port = sys.argv[1].split(':')
