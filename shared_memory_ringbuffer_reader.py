9ad37a07840e22821b52e222b3efb31f96bf5df2
Richard Campbell <richard.campbell@appliedoceansciences.com>
2025-04-21 19:00:47 -0700

    emit expected padding in weak main

--- shared_memory_ringbuffer_reader.py
+++ shared_memory_ringbuffer_reader.py
@@ -121,5 +121,8 @@ if __name__ == '__main__':
 
         for payload in shared_memory_ringbuffer_generator(shm_name):
             sys.stdout.buffer.write(payload)
+            padding_size = (len(payload) + 7) & ~7 - len(payload)
+            if padding_size:
+                sys.stdout.buffer.write(b'\0' * padding_size)
             sys.stdout.flush()
     main()
