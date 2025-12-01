70cbd13b1debfbdf308f1418815825f6c006b167
Richard Campbell <richard.campbell@appliedoceansciences.com>
2025-12-01 06:49:42 -0800

    rev from upstream

--- pcm2packets.py
+++ pcm2packets.py
@@ -6,7 +6,7 @@
 import struct
 import sys
 
-def pcm2packets(src, input_dtype_string, C, sample_rate):
+def pcm2packets(src, input_dtype_string, C, sample_rate, t0):
     itemsize = 2 if input_dtype_string == 'int16' else 4
 
     # number of samples per packet is maximum number s.t. packet size is not more than 1500 bytes
@@ -20,14 +20,24 @@ def pcm2packets(src, input_dtype_string, C, sample_rate):
 
     samples_yielded = 0
 
+    # convert floating point absolute time in unix seconds to integer number of 16-us ticks
+    t0_ticks = round(t0 * 1e6 / 16)
+
+    # number of 16-microsecond ticks
+    ticks_per_sample = 1e6 / (sample_rate * 16)
+
     while True:
         data_segment_bytes = src.read(itemsize * C * T)
         if len(data_segment_bytes) < itemsize * C * T: break
 
         samples_yielded += T
-        timestamp_ticks = round((t + samples_yielded / sample_rate) * 1e6 / 16) % 281474976710656
-        timestamp_lsbs = (timestamp_ticks) & 65535
-        timestamp_msbs = (timestamp_ticks) >> 16
+
+        # construct the unix time as a 48-bit number of 16-microsecond ticks in unix time
+        timestamp_ticks = (t0_ticks + round(samples_yielded * ticks_per_sample)) % (1 << 48)
+
+        # break the 48-bit time down into 16 lsbs and 32 msbs
+        timestamp_lsbs = timestamp_ticks & 65535
+        timestamp_msbs = timestamp_ticks >> 16
 
         logging_header_bytes = struct.pack('<HHI',
             packet_size, timestamp_lsbs, timestamp_msbs)
@@ -42,18 +52,21 @@ def pcm2packets(src, input_dtype_string, C, sample_rate):
 
         seqnum = (seqnum + 1) % 65536
 
-input_dtype_string = 'single'
-sample_rate = 24000.0
-C = 20
-t = 1725898437.0
+def main():
+    input_dtype_string = 'int16'
+    sample_rate = 31250.0
+    C = 1
+    t0 = 1725898437.0
 
-# loop over pairs of arguments
-for key, value in zip(sys.argv[1::2], sys.argv[2::2]):
-    if key == 'fs': sample_rate = float(value)
-    if key == 'C': C = int(value)
-    if key == 'dtype': input_dtype_string = value
-    if key == 't0': t = float(value)
+    # loop over pairs of arguments
+    for key, value in zip(sys.argv[1::2], sys.argv[2::2]):
+        if key == 'fs': sample_rate = float(value)
+        if key == 'C': C = int(value)
+        if key == 'dtype': input_dtype_string = value
+        if key == 't0': t0 = float(value)
 
-for logging_header_bytes, packet_bytes, padding in pcm2packets(sys.stdin.buffer, input_dtype_string, C, sample_rate):
-    sys.stdout.buffer.write(logging_header_bytes + packet_bytes + padding)
-    sys.stdout.buffer.flush()
+    for logging_header_bytes, packet_bytes, padding in pcm2packets(sys.stdin.buffer, input_dtype_string, C, sample_rate, t0):
+        sys.stdout.buffer.write(logging_header_bytes + packet_bytes + padding)
+        sys.stdout.buffer.flush()
+
+main()
